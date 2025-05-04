00fb05da76f2fdc54439a9ccf220edfdcf95af66
Aindrila Ray <aindrilaray0803@gmail.com>
2025-05-04 13:56:27 +0530

    mongodb-database connected

--- src/main/java/dev/ray/movies/MoviesApplication.java
+++ src/main/java/dev/ray/movies/MoviesApplication.java
@@ -3,8 +3,11 @@ package dev.ray.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class MoviesApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);

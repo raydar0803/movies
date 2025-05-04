dfbc5a8b33e8a3633950b230e3051b52d180b233
Aindrila Ray <aindrilaray0803@gmail.com>
2025-05-04 13:39:02 +0530

    initial-commit

---
+++ src/test/java/dev/ray/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ray.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

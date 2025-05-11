f714ead2246d540c482541f85d28c6f97783608e
Aindrila Ray <aindrilaray0803@gmail.com>
2025-05-11 20:09:37 +0530

    controller added for movies

---
+++ src/main/java/dev/ray/controller/MovieController.java
@@ -0,0 +1,17 @@
+package dev.ray.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public String getAllMovies() {
+        return "All movies!";
+    }
+    
+    
+}
